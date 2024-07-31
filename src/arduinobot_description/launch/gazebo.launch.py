11e7ca4c40af0b483da6d545d05cef4b91ae886a
Kevin Wang <k.wang1502483@gmail.com>
2024-07-30 22:47:07 -0700

    added camera

--- src/arduinobot_description/launch/gazebo.launch.py
+++ src/arduinobot_description/launch/gazebo.launch.py
@@ -56,12 +56,12 @@ def generate_launch_description():
         ,
         launch_arguments={'gz_args': ['-r -s -v1 ']}.items()
     )
-    # start_gazebo_client = IncludeLaunchDescription(
-    #     PythonLaunchDescriptionSource(
-    #         os.path.join(ros_gz_sim, 'launch', 'gz_sim.launch.py')
-    #     ),
-    #     launch_arguments={'gz_args': '-g -v1 '}.items()
-    # )
+    start_gazebo_client = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(ros_gz_sim, 'launch', 'gz_sim.launch.py')
+        ),
+        launch_arguments={'gz_args': '-g -v1 '}.items()
+    )
 
 
     spawn_robot = Node(
@@ -80,7 +80,7 @@ def generate_launch_description():
         robot_state_publisher_node,
         # gz_sim,
         start_gazebo_server,
-        # start_gazebo_client,
+        start_gazebo_client,
         spawn_robot
     ])
         
